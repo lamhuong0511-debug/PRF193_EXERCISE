d3fd3efbb78d447055d524e3643f6d3988abae7d
lamhuong0511-debug <lamhuong0511@gmail.com>
2025-09-29 21:53:07 +0700

    Add files via upload

---
+++ ex3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int a, count = 0;
+    scanf("%d",&a);
+    for ( int i=1; i<=a ; i++){
+        if (a % i == 0){
+            count++;
+        }
+    }
+    printf ("%d ", count);
+    return 0;
+}
